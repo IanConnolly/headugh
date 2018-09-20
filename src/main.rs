7219da188796f7f42cfdc1cb3bf2aa295f8a07db
Ian Connolly <ian@connolly.io>
2018-09-19 20:37:28 -0700

    Add `clap` dependency

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 extern crate libheadugh;
 
+#[macro_use]
+extern crate clap;
+
 use libheadugh::run;
 
 fn main() -> std::io::Result<()> {
-    let filename = std::env::args().nth(1).unwrap();
-    run(&filename)
+    let matches = clap_app!(headugh =>
+        (version: "0.1.0")
+        (author: "Ian Connolly <ian@connolly.io>")
+        (about: "\nyet another brainfuck interpreter")
+        (@arg INPUT: +required "sets the brainfuck file to execute")
+    ).get_matches();
+    run(matches.value_of("INPUT").unwrap())
 }
